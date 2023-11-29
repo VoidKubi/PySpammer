9e5cd3987b338c136f12fc0cbfc254df1c6d4b73
Kubi <131853348+VoidKubi@users.noreply.github.com>
2023-11-29 19:54:05 +0100

    Code

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import time
+import pyautogui as pygui
+
+
+print("Spammer by @VoidKubi")
+print(" ")
+
+co = input("Jakie słowo? ")
+ile = int(input("Ile razy? "))
+
+print("Spam zacznie się za 5 sekund.")
+
+time.sleep(5)
+
+for i in range(ile):
+    pygui.typewrite(co)
+    pygui.press("enter")
